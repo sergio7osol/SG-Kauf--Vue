3b6ba36921a11961acc8bb10bb838215ab1d711a
Sergey Osokin <sergey8osokin@gmail.com>
2021-01-04 15:23:56 +0100

    Created basic project structure for future Vue app.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container spend-track">
+    <div class="row">
+      <!-- <nav class="col-3" data-spend-track-wr="LeftMenu">
+        <left-menu class="spend-track__left-menu" :items='buyItems' title="Vertical Menu" />
+      </nav> -->
+      <div class="col-9" data-spend-track-wr="Content">
+          <div class="col-8">
+            <!-- <data-sender btnColor="#c36f9e" @start-sending="started" @end-sending="ended" /> -->
+            <h2>{{title}}</h2>
+            <img alt="Vue logo" src="./assets/logo.png">
+            <add-item />
+            <buy-list :items="buyItems" />
+          </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import LeftMenu from './components/LeftMenu.vue';
+import BuyList from './components/BuyList.vue';
+import AddItem from './components/AddItem.vue';
+// import DataSender from './components/DataSender.vue';
+// data:
+import buyItems from './data/buy-list.json'; 
 
 export default {
   name: 'App',
+  data() {
+    return {
+      title: 'SG-Kauf--Vue',
+      buyItems,
+      start: null
+    };
+  },
+  methods: {
+    reverseMessage() {
+      this.newText = this.newText
+        .split('')
+        .reverse()
+        .join('');
+    },
+    started(e) {
+      this.start = e;
+      console.log('Start sending data: ', e);
+    },
+    ended() {
+      const responseLasted = Date.now() - this.start;
+      console.log('Finish sending data. Lasted: ', responseLasted);
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    // LeftMenu,
+    AddItem,
+    BuyList,
+    // DataSender
+  },
+};
 </script>
 
-<style lang="scss">
-#app {
+<style>
+#spend-track {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
