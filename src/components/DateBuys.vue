180e62111bc0a2fdde0e1cc01fc74c3853cbbf21
Sergey Osokin <sergey8osokin@gmail.com>
2021-01-13 22:58:57 +0100

    Created the component for collecting all buys: 	new file:   src/components/DateBuys.vue

---
+++ src/components/DateBuys.vue
@@ -0,0 +1,20 @@
+<template>
+    <buy v-for="(buy, i) in buys" buy="buy" :key="i" />
+</template>
+
+<script>
+import Buy from './Buy.vue';
+
+export default {
+  name: 'date-buys',
+  props: {
+    buys: Array 
+  },
+  components: {
+      Buy
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
