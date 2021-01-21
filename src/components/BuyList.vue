af3d43c722e1d4eaad44f557d046246a93c8a5b3
Sergey Osokin <sergey.osokin@netfire.eu>
2021-01-22 00:39:16 +0100

    GREAT FIX! Fixed rerendering the components in a list using - :key="Date.now() + i" -> a really unique KEY, which is the main cause for rerendering: 	modified:   src/components/BuyList.vue

--- src/components/BuyList.vue
+++ src/components/BuyList.vue
@@ -2,7 +2,7 @@
   <div class="buy-list pt-3">
     <buy :buy="emptyBuy" isDefault />
     <ul class="list-group list-group-flush buy-list__items">
-      <buy v-for="(buy, i) in dateBuys" :buy="buy" :key="i" />
+      <buy v-for="(buy, i) in dateBuys" :buy="buy" :key="Date.now() + i" />
     </ul>
   </div>
 </template>
