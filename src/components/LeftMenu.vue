12fca23c2f10dcc9c1ea36cd503d6b16c4bc7610
Sergey Osokin <sergey.osokin@netfire.eu>
2021-01-30 15:32:59 +0100

    Styled the LeftMenu component: 	modified:   src/components/LeftMenu.vue

--- src/components/LeftMenu.vue
+++ src/components/LeftMenu.vue
@@ -1,9 +1,9 @@
 <template>
-  <nav id="sidebarMenu" class="d-md-block bg-light sidebar collapse vertical-menu">
+  <nav id="sidebarMenu" class="d-md-block bg-dark sidebar collapse vertical-menu">
       <div class="position-sticky pt-3">
         <ul class="nav flex-column vertical-menu__list">
-          <li class="nav-item vertical-menu__item" :class="{'vertical-menu__item--active': date === activeItem}" v-for="date in dates" :key="date">
-            <a class="nav-link active vertical-menu__item-link" @click="chooseDate(date)" aria-current="page" href="#">
+          <li class="nav-item vertical-menu__item" v-for="date in dates" :key="date">
+            <a class="nav-link vertical-menu__item-link" :class="{'vertical-menu__item-link--active': date === activeItem}" @click="chooseDate(date)" aria-current="page" href="#">
               <svg xmlns="http://www.w3.org/2000/svg" class="feather feather-shopping-cart vertical-menu__item-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
               <span class="vertical-menu__item-text">{{date}}</span>
             </a>
@@ -48,9 +48,10 @@ export default {
     margin: 40px 0 0;
   }
   .vertical-menu {
+    background-color: #2E2E2E;
     padding: 0;
+    min-height: calc(100vh - 2.5rem);
     &__item {
-      border-bottom: 1px solid #aaa;
       &--active {
         background-color: #dde;  
         border: 1px solid #bb88;
@@ -58,11 +59,43 @@ export default {
     }
     &__item-link {
       display: flex;
-      padding-left: .5rem;
-      justify-content: space-between;
+      align-items: center;
+      height: 61px;
+      position: relative;
+      z-index: 2;
+      padding: 20px 30px 19px 70px;
+      font-size: 18px;
+      font-family: UniversLT-Condensed, "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
+      font-weight: normal;
+      font-style: normal;
+      color: #fff;
+      text-decoration: none;
+      text-transform: uppercase;
+      border-bottom: 1px solid #777;
+      transition: background-color 0.3s, color 0.3s;
+
+      &:hover {
+        background: #232323;
+        color: #0070BA;
+      }
+
+      &--active {
+        background: #0070BA !important;
+        color: #fff !important;
+
+        &::after {
+          content: "";
+          position: absolute;
+          right: -17px;
+          border-color: #0000 #0000 #0000 #0070BA;
+          border-width: 30px 0 30px 17px;
+          border-style: solid;
+        }
+      }
     }
     &__item-icon {
-     margin-right: .5rem;
+      position: absolute;
+      left: 1rem;
     }
   }
 </style>
