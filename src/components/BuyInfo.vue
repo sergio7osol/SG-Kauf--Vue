5d41f6bc00d6106f174b996b351caa0704c8444d
Sergey Osokin <sergey.osokin@netfire.eu>
2021-04-04 02:05:41 +0200

    Added some more sample shops: 	modified:   src/components/BuyInfo.vue

--- src/components/BuyInfo.vue
+++ src/components/BuyInfo.vue
@@ -68,11 +68,11 @@ export default {
         localProducts: this.products,
         ValueCollection: {
             countries: ['Germany', 'Russia', 'online'],
-            shopNames: ['REWE', 'ALDI', 'Kaufland', 'Lidl', 'PENNY', 'Amazon.de', 'Netflix.com', 'Edeka', 'ROHLFS BÄCKEREI KONDITOREI GmbH', 'Apotheke a.d. Friedenseiche Nikolaus Wendel', 'About you', 'Netflix', 'Innovativelanguage.com', 'Mango', 'OVB', 'Vodavone GmbH', 'Telekom Deutschland GmbH', 'Ernst Scholz', 'Sparkasse'], 
-            indexes: ['22307', '22529', '22299', '20251', '22761', '22301', '20249', '22525', '22041', 'online'],
+            shopNames: ['REWE', 'ALDI', 'Kaufland', 'Lidl', 'PENNY', 'Amazon.de', 'Netflix.com', 'Edeka', 'ROHLFS BÄCKEREI KONDITOREI GmbH', 'Apotheke a.d. Friedenseiche Nikolaus Wendel', 'About you', 'Netflix', 'Innovativelanguage.com', 'Mango', 'OVB', 'Vodavone GmbH', 'Telekom Deutschland GmbH', 'Ernst Scholz', 'Sparkasse', 'Apotheke in der Marktplatz Galerie'], 
+            indexes: ['22307', '22529', '22299', '20251', '22761', '22301', '20249', '22525', '22041', '22177', 'online'],
             cities: ['Hamburg', 'Moscow', 'Saransk', 'online'],
-            streets: ['Fuhlsbuettler Str.', 'Troplowitzstrasse', 'Osterfeldestrasse', 'Winterhuder Marktplatz', 'Eppendorfer Marktplatz', 'Stresemannstrasse', 'Nedderfeld', 'Dorotheenstrasse', 'Kümmellstraße', 'Kieler Straße', 'Grelckstraße', 'Walddörferstraße', 'Eppendorfer Baum', 'online'],
-            houseNumbers: ['387', '8', '7', '13-15', '2', '39', '34',  '35', '300', '70', '116-122', '4-8', '30-40', '595', '579', '146', 'online'],
+            streets: ['Fuhlsbuettler Str.', 'Troplowitzstrasse', 'Osterfeldestrasse', 'Winterhuder Marktplatz', 'Eppendorfer Marktplatz', 'Stresemannstrasse', 'Nedderfeld', 'Dorotheenstrasse', 'Kümmellstraße', 'Kieler Straße', 'Grelckstraße', 'Walddörferstraße', 'Eppendorfer Baum', 'Bramfelder Chaussee', 'online'],
+            houseNumbers: ['387', '8', '7', '13-15', '2', '39', '34',  '35', '300', '70', '116-122', '4-8', '30-40', '595', '579', '146', '230', 'online'],
             currencies: ['EUR', 'RUB'],
             payMethods: ['EC card', 'Cash', 'N26 card', 'PayPal']
         }
